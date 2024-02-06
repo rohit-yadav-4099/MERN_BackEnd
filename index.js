38c8478c45a82321956ae8b6b8026425694ab033
rohit-yadav-4099 <rohitjnp2194@gmail.com>
2024-02-06 13:05:46 +0530

    done

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// app.use(express.json({ limit: '10mb' }));
 app.use(express.json());
 
 // app.use(cors());
@@ -37,18 +36,6 @@ app.use("/api", routeone);
 app.use("/api", productRoute);
 app.use("/api",courseRoute)
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // You can also allow multiple origins by separating them with commas:
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, http://example.com');
-  
-  // Allow other necessary headers
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  next();
-})
 
 
 // router.post("/api/save-data", async(req, res) => {
